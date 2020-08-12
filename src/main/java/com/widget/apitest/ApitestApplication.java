539d643c362046edd6cf27cd56eb7352422d240f
Jonathan Sanchez <me-gitlab@jonathan-sanchez.com>
2020-08-12 11:47:58 +0200

    project init

---
+++ src/main/java/com/widget/apitest/ApitestApplication.java
@@ -0,0 +1,13 @@
+package com.widget.apitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitestApplication.class, args);
+	}
+
+}

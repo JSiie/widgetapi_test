adfcb01c90ecdb120e2a7ff3a42d750895b299a6
Jonathan Sanchez <me-gitlab@jonathan-sanchez.com>
2020-08-13 20:50:03 +0200

    adding more complex tests. need to rewirte it in 2 files

--- src/test/java/com/widget/apitest/TestWebApp.java
+++ src/test/java/com/widget/apitest/TestWebApp.java
@@ -6,13 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.jayway.jsonpath.JsonPath;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -66,7 +69,96 @@ public class TestWebApp {
                 .andExpect(jsonPath("$.y").value(1))
                 .andExpect(jsonPath("$.width").value(100))
                 .andExpect(jsonPath("$.height").value(150))
-                .andExpect(jsonPath("$.zindex").value(0))
+                //widget with index 4 was created before, therefore this one will be on foreground
+                .andExpect(jsonPath("$.zindex").value(5))
+                ;
+    }
+    
+    @Test
+    public void createWidgetWithIndex()
+      throws Exception {   
+        this.mockMvc.perform(post("/widget")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"x\":1,\"y\":1,\"width\":100,\"height\":150,\"zindex\":3}}")
+                            )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.x").value(1))
+                .andExpect(jsonPath("$.y").value(1))
+                .andExpect(jsonPath("$.width").value(100))
+                .andExpect(jsonPath("$.height").value(150))
+                .andExpect(jsonPath("$.zindex").value(3))
+                ;
+    }
+    
+    @Test
+    public void ListWidgets()
+      throws Exception {   
+        this.mockMvc.perform(get("/widget")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$[0].id").isNotEmpty())
+                .andExpect(jsonPath("$[0].x").value(1))
+                .andExpect(jsonPath("$[0].y").value(1))
+                .andExpect(jsonPath("$[0].width").value(100))
+                .andExpect(jsonPath("$[0].height").value(150))
+                .andExpect(jsonPath("$[0].zindex").value(3))
+                ;
+    }
+    
+    @Test
+    public void UpdateWidget()
+      throws Exception {   
+        MvcResult result = this.mockMvc.perform(post("/widget")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"x\":10,\"y\":10,\"width\":200,\"height\":250,\"zindex\":3}}")
+                            )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.x").value(10))
+                .andExpect(jsonPath("$.y").value(10))
+                .andExpect(jsonPath("$.width").value(200))
+                .andExpect(jsonPath("$.height").value(250))
+                .andExpect(jsonPath("$.zindex").value(3))
+                .andReturn()
+                ;
+        
+        String id = JsonPath.read(result.getResponse().getContentAsString(), "$.id");
+        
+        this.mockMvc.perform(put("/widget")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"x\":30,\"zindex\":1}}")
+                            )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.x").value(30))
+                .andExpect(jsonPath("$.y").value(10))
+                .andExpect(jsonPath("$.width").value(200))
+                .andExpect(jsonPath("$.height").value(250))
+                .andExpect(jsonPath("$.zindex").value(1))
+                ;
+        
+        this.mockMvc.perform(get("/widget")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$[0].id").value(id))
+                .andExpect(jsonPath("$[0].x").value(30))
+                .andExpect(jsonPath("$[0].y").value(10))
+                .andExpect(jsonPath("$[0].width").value(200))
+                .andExpect(jsonPath("$[0].height").value(250))
+                .andExpect(jsonPath("$[0].zindex").value(1))
                 ;
     }
 
